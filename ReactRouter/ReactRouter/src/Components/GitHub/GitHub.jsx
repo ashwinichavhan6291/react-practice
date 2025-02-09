ffb3277ed566f9217071c8be1f6e1255e13d9cab
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2025-02-09 14:34:39 +0530

    fetch my api

--- ReactRouter/ReactRouter/src/Components/GitHub/GitHub.jsx
+++ ReactRouter/ReactRouter/src/Components/GitHub/GitHub.jsx
@@ -4,17 +4,11 @@ import { useLoaderData } from 'react-router-dom'
 function GitHub() {
 
     const data=useLoaderData("");
-// let[data,setData]=useState([]);
-//     useEffect(() => {
-      
-//         fetch("https://api.github.com/users/hiteshchoudhary").then(response=>response.json()).then(data=>{console.log(data);
-//             setData(data)
-//         })
-       
-//     }, [])
+    console.log(data);
+
     
   return (
-    <div className='text-center m-5 p-4 bg-gray-700 text-white text-3xl'>Github followers : {data.followers}
+    <div className='text-center m-5 p-4 bg-gray-700 text-white text-3xl'>Github login : {data.login}
 <img src={data.avatar_url} alt="Github Picture" width={300}/>
     </div>
 
@@ -26,7 +20,7 @@ export default GitHub
 
 export const githubLoader=()=>{
 
-    const respone=fetch("https://api.github.com/users/hiteshchoudhary");
+    const respone=fetch("https://api.github.com/users/ashwinichavhan6291");
     return respone;
 
 }
