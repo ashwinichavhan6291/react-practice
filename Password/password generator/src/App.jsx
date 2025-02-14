75b529dcca7e3f5a6905a5d374eef7c790b4ebc2
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2025-02-14 16:03:35 +0530

    unnecessory use of select function

--- Password/password generator/src/App.jsx
+++ Password/password generator/src/App.jsx
@@ -5,46 +5,39 @@ import { useEffect } from "react";
 import { useRef } from "react";
 
 function App() {
+let passwordRef=useRef(null);
+ let[password,setPassword]=useState("");
+ let[length,setLength]=useState(8);
+ let[number,setNumber]=useState(false);
+ let[character,setCharacter]=useState(false);
 
-  const passwordRef=useRef(null);
-  let [number, setNumber] = useState(false);
-  let [character, setCharacter] = useState(false);
-  let [length, setLength] = useState(8);
+ const passwordGenerator=useCallback(()=>{
+  let pass="";
+  let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw x y z";
+  if(number) str+="0123456789";
+  if(character) str+= "$%^*@!";
 
-  let [password, setPassword] = useState("");
-
-  const passwordgenerator = useCallback(() => {
-    let pass = "";
-    let str =
-      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw x y z";
-
-    if (number) str += "0123456789";
-    if (character) str += "!@#$%^*&()_+{}:";
-
-    for (let i = 1; i <=length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
-      pass += str.charAt(char);
-    }
-    setPassword(pass);
-  }, [length, number, character,setPassword]);
+  for(let i=1;i<=length;i++){
+    let char=Math.floor(Math.random() * str.length+1);
+   
+     pass+=str.charAt(char);
+ 
+  }
+ return pass;
+ },[length,character,number])
 
 useEffect(()=>{
-  passwordgenerator();
-},[length,number,character,passwordgenerator])
-
-// passwordgenerator();
-
-
+  setPassword((passwordGenerator));
+},[passwordGenerator])
 
 const copyPassword=useCallback(()=>{
-  passwordRef.current?.focus();
-passwordRef.current?.select();
-passwordRef.current?.setSelectionRange(0, 5);
-const partialPassword = password.slice(0, 5);
-
+passwordRef.current?.focus();
+passwordRef.current?.setSelectionRange(0,5);
+let partialPassword=password.slice(0,5);
 window.navigator.clipboard.writeText(partialPassword);
-},[password])
 
+
+},[password])
   return (
     <>
       <div className="w-full max-w-md shadow-md rounded-lg px-4 mx-20 my-8 text-oranage-500 bg-gray-700">
@@ -54,14 +47,14 @@ window.navigator.clipboard.writeText(partialPassword);
         <div className="flex-shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
-            ref={passwordRef}
-            value={password}
+           value={password}
+           ref={passwordRef}
             className="outline-none w-full py-1 px-3 bg-white"
             placeholder="password"
             readOnly
           />
 
-          <button onClick={copyPassword}className="outline-none text-white bg-blue-700 shrink-0 px-3 py-0.5">
+          <button className="outline-none text-white bg-blue-700 shrink-0 px-3 py-0.5" onClick={copyPassword}>
             Copy
           </button>
         </div>
@@ -73,10 +66,9 @@ window.navigator.clipboard.writeText(partialPassword);
               min={6}
               max={100}
               value={length}
+              onChange={(e)=>setLength(e.target.value)}
               className="cursor-pinter"
-              onChange={(e) => {
-                setLength(e.target.value);
-              }}
+             
             />
             <label htmlFor=""> Length : {length}</label>
           </div>
@@ -84,11 +76,10 @@ window.navigator.clipboard.writeText(partialPassword);
           <div className="flex items-center gap-x-1">
             <input
               type="checkbox"
-              defaultChecked={number}
-              id="numberInput"
-              onChange={() => {
-                setNumber((prev) => !prev);
-              }}
+             defaultChecked={number}
+         
+             id="numberInput"
+             onChange={()=>setNumber((prev)=>!prev)}
             />
             <label htmlFor="numberInput">Numbers</label>
           </div>
@@ -96,11 +87,9 @@ window.navigator.clipboard.writeText(partialPassword);
           <div>
             <input
               type="checkbox"
-              defaultChecked={character}
+            defaultChecked={character}
               id="characterInput"
-              onChange={() => {
-                setCharacter((prev) => !prev);
-              }}
+              onChange={()=>setCharacter((prev)=>!prev)}
             />
             <label htmlFor="characterInput">Characters</label>
           </div>
