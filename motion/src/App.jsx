49f4adbc0e44e92e0dfbc571f07b0017c46aca67
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2025-03-24 15:26:39 +0530

    key frames animation

--- motion/src/App.jsx
+++ motion/src/App.jsx
@@ -9,33 +9,22 @@ function App() {
    
      <div className="w-screen" >
       
-      <motion.h1
-      initial={{
-    x:0
-      }}
-      animate={{
-        x:1000,
-        scale:3
-      }}
-      transition={{
-        duration:5,
-        
-      }}
-      >hellow!</motion.h1>
-      <motion.img  src="https://www.partysuppliesindia.com/cdn/shop/products/1_36_9f92dde3-d77d-4459-a21f-63744a94c836.jpg?v=1735574298&width=1500"
-      className='w-32 h-32 mt-10' 
+     
+      <motion.div  
+      className='w-32 h-32 mt-10 bg-gray-300' 
       
-      animate={{x:1000,
-       rotate:360
+      animate={{x:[0,800,800,0,0],
+        y:[0,0,300,300,0],
+        rotate:[0,360,-360,0]
+      
  }} transition={{
-  duration:3,
+  duration:4,
   delay:2,
-  repeat:2,
-  ease:'anticipate'
+  
  }}
  >
        
-      </motion.img>
+      </motion.div>
      </div>
     </>
   )
