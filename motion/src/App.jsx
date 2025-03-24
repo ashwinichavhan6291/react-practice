fab55bb0fa1ce68b3f631da9fb064a6f13517158
ashwinichavhan6291 <ashwinichavhan6291@gmail.com>
2025-03-24 15:57:12 +0530

    drag

--- motion/src/App.jsx
+++ motion/src/App.jsx
@@ -12,10 +12,18 @@ function App() {
      
       <motion.div  
       className='w-32 h-32 mt-10 bg-gray-300' 
-      
+      drag
+      whileDrag={{scale:0.8}}
+      dragConstraints={{
+        left:0,
+        top:0,
+        right:1000,
+        bottom:500
+      }}
+      dragDirectionLock="true"
   whileHover={{background:"gray"}}
   whileTap={{scale:0.7}}
-whileInView={{background:"red"}}
+
  >
        
       </motion.div>
